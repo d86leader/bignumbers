22ebc005d55c148db2eeb58e1d4f9437505b28cf
d86leader <d86leader@gmail.com>
2018-02-14 12:45:42 +0700

    wrote required tests in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 #include "bignumbers.h"
 
@@ -9,30 +11,33 @@ using std::endl;
 int main(int argc, char** _argv)
 {
 //	vector<string> argv (_argv, _argv + argc);
+	srand(time(nullptr));
 
 	bignumber a;
 	bignumber b;
 
 	for (int i = 0; i < 1000; ++i)
 	{
-		const auto asize = 10000;
-		const auto bsize = 500;
+		if (i % 50 == 0)
+		{
+			std::cout << "on test number "<<i<<std::endl;
+		}
+		const size_t asize = rand()%1000;
+		const size_t bsize = rand()%1000;
 
 		a = bignumber::generate(asize);
 		b = bignumber::generate(bsize);
 
 		auto t = a.quot_rem(b);
+		auto quot = t.first;
+		auto rem = t.second;
 
-		auto q = t.first;
-		auto r = t.second;
-
-		auto m = q * b;
-		auto s = m + r;
-
-		assert (s == a);
-		std::cout << "all ok!\n";
+		assert( quot*b + rem == a );
+		assert( quot*b == a - rem );
+		assert( a == a );
+		assert( rem < b );
 	}
-
+	std::cout << "all tests passed successfully\n";
 
 	return 0;
 }
