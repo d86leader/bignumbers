6741b53b6338662a32f3bc11a7125ca47c1a3b43
d86leader <d86leader@gmail.com>
2018-02-14 12:39:12 +0700

    fixed usage of static function

--- main.cpp
+++ main.cpp
@@ -8,18 +8,18 @@ using std::endl;
 
 int main(int argc, char** _argv)
 {
-	vector<string> argv (_argv, _argv + argc);
+//	vector<string> argv (_argv, _argv + argc);
 
 	bignumber a;
 	bignumber b;
 
-	for (int i = 0; i < 100; ++i)
+	for (int i = 0; i < 1000; ++i)
 	{
 		const auto asize = 10000;
 		const auto bsize = 500;
 
-		a.generate(asize);
-		b.generate(bsize);
+		a = bignumber::generate(asize);
+		b = bignumber::generate(bsize);
 
 		auto t = a.quot_rem(b);
 
