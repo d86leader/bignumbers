b3fb9ee6baa132782ba482dddad65f592189e56b
d86leader <d86leader@mail.com>
2018-02-14 22:04:48 +0700

    added sign handling to dividing

---
+++ mod-test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Big.h"
+
+int main()
+{
+	Big a = -10;
+	Big b = 3;
+	std::cout << a.dump() << " / " << b.dump() << ":\n";
+	auto t = a.quot_rem(b);
+	std::cout << t.first.dump() << std::endl;
+	std::cout << t.second.dump() <<std::endl;
+
+	return 0;
+}
