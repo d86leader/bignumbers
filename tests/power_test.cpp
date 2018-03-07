fdb2124292caa912b77672cf5dba209a483849db
d86leader <d86leader@gmail.com>
2018-03-07 13:32:42 +0700

    power test now uses modular exp as well

--- tests/power_test.cpp
+++ tests/power_test.cpp
@@ -13,20 +13,18 @@ int main(int argc, char** _argv)
 	std::vector<std::string> argv (_argv, _argv + argc);
 	srand(time(nullptr));
 
-	const size_t base_size = rand()%4 + 1;
-	const size_t exp_size  = 1;
+	cout << "require 'openssl'\n";
 
+	const size_t base_size = rand()%100 + 1;
+	const size_t exp_size  = rand()%100 + 1;
+
+	Big modulo = 1000000007ll;
 	Big base = Big::generate(base_size);
 	Big exp  = Big::generate(exp_size);
-	Big result = base.exp(exp);
 
-	cout << base.dump(false) << "**" << exp.dump(false) << "==" << result.dump(false);
+	Big result = base.exp(exp, modulo);
 
-	base = Big::generate(1);
-	exp  = Big::generate(2);
-	result = base.exp(exp);
-
-	cout << "&&" << base.dump(false) << "**" << exp.dump(false) << "==" << result.dump(false) << endl;
+	cout << "puts(" << base.dump(false) << ".to_bn.mod_exp(" << exp.dump(false) << " , " << modulo.dump(false) << ") == " << result.dump(false) << ")\n";
 
 	return 0;
 }
