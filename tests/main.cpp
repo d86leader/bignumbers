829575daca7b46d339a76b0649ff6c90936d8a33
d86leader <d86leader@mail.com>
2018-04-27 23:41:24 +0700

    some modifications to main-test making it easier to configure

--- tests/main.cpp
+++ tests/main.cpp
@@ -10,8 +10,10 @@ int main(int argc, char** _argv)
 	Big::generator_type gen;
 	Big::distribution_type dist;
 
-	constexpr size_t min_size = 1;
+	constexpr size_t min_size = 2;
 	constexpr size_t max_size = 1000;
+	constexpr size_t tries = 10000;
+	constexpr size_t print_each = tries / 10;
 	std::uniform_int_distribution<size_t> size_distr (min_size, max_size);
 
 	Big a;
@@ -20,9 +22,9 @@ int main(int argc, char** _argv)
 
 	try
 	{
-		for (int i = 0; i < 1000; ++i)
+		for (int i = 0; i < tries; ++i)
 		{
-			if (i % 50 == 0)
+			if (i % print_each == 0)
 			{
 				std::cout << "on test number "<<i<<std::endl;
 			}
